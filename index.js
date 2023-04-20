7bc4791dd6299cf138a7d033e6420f5d6554890a
joshua <josh.newbill08@hotmail.com>
2023-04-19 19:52:15 -0500

    added

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
+
+
 // Write your code here
+
+//Create a variable called multiply set to an equation that will multiply the variables num1 and 
+//num2; the result of the multiplication should be 62.
+let num1=2;
+let num2=31;
+
+let multiply = num1 * num2
+
+//Create a variable called random that will generate a random integer greater than 0.
+
+let random = Math.ceil(Math.random())
+
+//Create a variable called mod set to an equation that will calculate the 
+//remainder of dividing variable num3 by num4; the remainder should be 4.
+
+const num3=53;
+const num4=7;
+
+
+let mod = num3 % num4;
+
+//Create a variable called max that finds the highest number in a 
+//set; the value returned should be 20.
+
+let max = Math.max(12,3,12,20,13,7,5,14,5,2,3,4,5);
